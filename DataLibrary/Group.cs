5f6871a9c0164920bc0ff19828d494acb337a826
Bruno Glogović <bglogov@algebra.hr>
2025-06-27 01:44:20 +0200

    Add class Group and Team from group_results.json

---
+++ DataLibrary/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class Group
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("letter")]
+        public string Letter { get; set; } = string.Empty;
+
+        [JsonPropertyName("ordered_teams")]
+        public List<Team> OrderedTeams { get; set; } = new();
+    }
+}
