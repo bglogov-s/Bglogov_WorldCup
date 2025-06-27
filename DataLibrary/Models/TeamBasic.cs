63ba079194983c2d485f3eeb95162fc193d5ebdd
Bruno Glogović <bglogov@algebra.hr>
2025-06-27 02:00:04 +0200

    Add TeamBasic, TeamStanding classes

---
+++ DataLibrary/Models/TeamBasic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    public class TeamBasic
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; } = string.Empty;
+
+        [JsonPropertyName("alternate_name")]
+        public string? AlternateName { get; set; }
+
+        [JsonPropertyName("fifa_code")]
+        public string FifaCode { get; set; } = string.Empty;
+
+        [JsonPropertyName("group_id")]
+        public int GroupId { get; set; }
+
+        [JsonPropertyName("group_letter")]
+        public string GroupLetter { get; set; } = string.Empty;
+    }
+}
